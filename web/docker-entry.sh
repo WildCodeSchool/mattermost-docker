37aa1d8741a0ed1a59d0e89eef10bd1ae047affc
Em-AK <emilien.ak@gmail.com>
2016-08-22 17:02:58 +0200

    Fix - let letsencrypt validate cert on first run

--- web/docker-entry.sh
+++ web/docker-entry.sh
@@ -2,8 +2,16 @@
 echo Starting Nginx
 sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost
 sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost-ssl
-if [ "$MATTERMOST_ENABLE_SSL" = true ]; then
-    ssl="-ssl"
+sed -Ei "s/DOMAIN/$DOMAIN/" /etc/nginx/sites-available/letsencrypt
+sed -Ei "s/DOMAIN/$DOMAIN/" /etc/nginx/sites-available/mattermost-ssl
+
+if test -f "/etc/letsencrypt/live/$DOMAIN/cert.pem"; then
+    if [ "$MATTERMOST_ENABLE_SSL" = true ]; then
+	ssl="-ssl"
+    fi
+    ln -s -f /etc/nginx/sites-available/mattermost$ssl /etc/nginx/sites-enabled/mattermost
+else
+    ln -s -f /etc/nginx/sites-available/letsencrypt /etc/nginx/sites-enabled/mattermost
 fi
-ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/sites-enabled/mattermost
+
 nginx -g 'daemon off;'
